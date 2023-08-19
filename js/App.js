a9e65055c4459e91bde336e69950091091cde863
FrostbyteFreddy <moritz.burn@gmail.com>
2023-08-19 15:18:39 +0200

    Update

---
+++ js/App.js
@@ -0,0 +1,55 @@
+(function($) {
+
+    console.log("test");
+
+    var MAIN = MAIN || {},
+
+    MAIN = {
+
+        _: function() {
+            this.header.init();
+            // Add more functions to include here
+        },
+
+        header: {
+            init: function() {
+                // Header specific initializations
+                this.handleHeaderState();
+            },
+
+            handleHeaderState: function() {
+                var threshold = 100; // Change this value based on your requirements
+
+                function setHeaderState() {
+
+                    console.log("test");
+                    var scrollPosition = $(window).scrollTop();
+
+                    if (scrollPosition > threshold) {
+                        $('header').attr('data-state', 'minimized');
+                    } else {
+                        $('header').attr('data-state', 'maximized');
+                    }
+                }
+
+                // Initial check
+                setHeaderState();
+
+                // Listen to the scroll event
+                $(window).on('scroll', setHeaderState);
+            }
+        },
+
+        // You can add other functionalities here, similar to the slider example you provided
+        // For example:
+        // contentElements: {
+        //     slider: {
+        //         // Slider functionalities
+        //     }
+        // }
+    }
+
+    // Initialize the functionalities
+    MAIN._();
+
+})(jQuery);
