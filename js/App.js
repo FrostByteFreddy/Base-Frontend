816a5670f4910688bd0f07ca8fe252fe6871b81d
FrostbyteFreddy <moritz.burn@gmail.com>
2023-08-22 23:27:09 +0200

    Update

--- js/App.js
+++ js/App.js
@@ -16,22 +16,35 @@
             },
 
             handleHeaderState: function() {
-                var threshold = 100; // Change this value based on your requirements
+
+                var threshold = 12.5; // The distance to scroll up or down to trigger the change
+                var lastScrollPosition = -1; // Initialize to -1 to ensure the first scroll event doesn't trigger the change
 
                 function setHeaderState() {
 
-                    console.log("test");
-                    var scrollPosition = $(window).scrollTop();
-
-                    if (scrollPosition > threshold) {
-                        $('header').attr('data-state', 'minimized');
-                    } else {
-                        $('header').attr('data-state', 'maximized');
+                    var currentScrollPosition = $(window).scrollTop();
+                    
+                    // If lastScrollPosition is -1, it's the first check, so just set the lastScrollPosition to the current value
+                    if (lastScrollPosition === -1) {
+                        lastScrollPosition = currentScrollPosition;
+                        return;
                     }
-                }
 
-                // Initial check
-                setHeaderState();
+                    var scrollDifference = Math.abs(currentScrollPosition - lastScrollPosition); // Calculate the difference between the current and last scroll position
+                    console.log(scrollDifference);
+                    // Check if the user has scrolled more than the threshold
+                    if (scrollDifference > threshold) {
+                        if (currentScrollPosition > lastScrollPosition) {
+                            // Scrolled down
+                            $('header').attr('data-state', 'minimized');
+                        } else {
+                            // Scrolled up
+                            $('header').attr('data-state', 'maximized');
+                        }
+                    }
+
+                    lastScrollPosition = currentScrollPosition; // Update the last scroll position
+                }
 
                 // Listen to the scroll event
                 $(window).on('scroll', setHeaderState);
